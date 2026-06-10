e766086039a3dc05383611f7b0db3aee87f13608
cleonavila <cleon@almagrario.com>
2026-06-10 09:02:11 -0500

    Ajuste estilos reviews

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './stupid/singleton.js';
+// import './clean-code/07-tarea';
+import './code-smells/02-low-coupling';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
